4ab3c77cf0a1257529be1e48e897dcb07c2d236a
github18122021 <vrajbagadiya0071@gmail.com>
2023-12-11 13:43:56 -0500

    useRef hook revision

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { useRef } from 'react';
 
 function App() {
+  let h1Element = useRef(null);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 ref = {h1Element} >Hello World</h1>
+      <button onClick={() => {
+        // alert(h1Element.current.value);
+        // console.log(h1Element.current.innerHTML);
+
+
+        // h1Element.current.innerHTML = '<h5>Helli World<h5/>';
+        // h1Element.current.style.backgroundColor = "Yellow";
+        // console.log(h1Element.current.style.backgroundColor);
+
+        h1Element.current.style.fontSize = "5rem";
+
+      }}> Click to Increase The size of Hello World</button>
     </div>
   );
 }
