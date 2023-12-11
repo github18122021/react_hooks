83e4b98b32456e7af03f616027d619d0a266b545
Vrajkumar Bagadiya <96316547+github18122021@users.noreply.github.com>
2023-12-11 14:40:45 -0500

    useReducer-Hook revised
    Built Counter feature for revision of useReducer-Hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import './App.css';
 import { useReducer } from 'react';
 
+// let letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+// letters = letters.split('');
+
+// let initialState = {
+//   letter : letters[0],
+// }
+
+let initialState = {
+  count : 0,
+}
+
+function reducer(state, action) {
+  switch(action.type) {
+    case "increase": 
+      return {count : state.count + 1};
+    case "decrease":
+      return {count: state.count - 1};
+    case "reset":
+      return {count: 0};
+    default: 
+      return state;  
+  }
+}
+
 
 function App() {
+  let [state, dispatch] = useReducer(reducer, initialState);
+
+  function increase() {
+    // console.log("increase");
+    dispatch({type : "increase"});
+  }
+
+  function decrease() {
+    // console.log("decrease");
+    dispatch({type : "decrease"});
+  }
   return (
     <div className="App">
-     
+     <h1>{state.count}</h1>
+     <button onClick = {increase}>Increase</button>
+     <button onClick = {decrease}>Decrease</button>
     </div>
   );
 }
